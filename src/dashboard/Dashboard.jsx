40160c2559a78ae144df302a6f0bc4589dbdfe48
sahil-mane <manesahil238@gmail.com>
2024-04-25 21:18:13 +0530

    eigth commit on client

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -6,11 +6,11 @@ import { useNavigate} from 'react-router-dom'
 
 const Dashboard = () => {
   const navigate = useNavigate();
-  const { user } = useContext(AuthContext)
+  const { user, loginwithGoogle } = useContext(AuthContext)
   return (
     <> 
     <div className="h-screen flex flex-col justify-center items-center lg:mx-56">
-      <div className="text-5xl font-bold px-24 my-3">Welcome {user.displayName}</div>
+      <div className="text-5xl font-bold px-24 my-3">Welcome {user.displayName || loginwithGoogle.email}</div>
       <div className="flex justify-center ">
         <button className="m-2 px-4 py-2 bg-gray-200" onClick={()=>{navigate('/admin/dashboard/upload')}}>Add Book</button>
         <button className="m-2 px-4 py-2 bg-gray-200" onClick={()=>{navigate('/admin/dashboard/manage')}}>Manage Book</button>
